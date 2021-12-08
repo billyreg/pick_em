9cbb37d1de909b5f07022639a00d151a11ec0db6
anonymous anonymous <bill.reggio@kellogg.northwestern.edu>
2021-12-08 02:45:10 +0000

    Display associated collections on show pages

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -42,8 +42,14 @@ class PicksController < ApplicationController
   # DELETE /picks/1
   def destroy
     @pick.destroy
-    redirect_to picks_url, notice: 'Pick was successfully destroyed.'
+    message = "Pick was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to picks_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
