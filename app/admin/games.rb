66c6a8ac7d7fb09ab5ae04189ceb8d9a882b2f57
anonymous anonymous <bill.reggio@kellogg.northwestern.edu>
2021-12-08 02:45:07 +0000

    Whitelist columns in Game for ActiveAdmin

--- app/admin/games.rb
+++ app/admin/games.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Game do
 
+ permit_params :favorite, :underdog, :description
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
