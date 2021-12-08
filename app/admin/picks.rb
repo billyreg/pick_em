16d8716a69f523944d6fe3d9025b80e2c9b389ef
anonymous anonymous <bill.reggio@kellogg.northwestern.edu>
2021-12-08 02:45:05 +0000

    Whitelist columns in Pick for ActiveAdmin

--- app/admin/picks.rb
+++ app/admin/picks.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Pick do
 
+ permit_params :user_id, :game_id, :team, :weight
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
